ca10f38b4223d32ae36154c24881cadf6a2b5cb7
bogdan <bb.japrog@gmail.com>
2015-08-09 12:26:02 +0300

    Первый комит, очень много работы, пока ничего нету

---
+++ app/src/main/java/com/example/bogdan/interval_trempo/ItemAdapter.java
@@ -0,0 +1,30 @@
+package com.example.bogdan.interval_trempo;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by bogdan on 08.08.2015.
+ */
+public class ItemAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
