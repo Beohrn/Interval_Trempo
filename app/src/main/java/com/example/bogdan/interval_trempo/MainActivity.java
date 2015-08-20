ee50425e82abc8293e6db5b158012d93d1f47cf4
Beohrn <beohrn1@gmail.com>
2015-08-20 12:23:30 +0300

    Добавлен EditText, который считывает цифры

--- app/src/main/java/com/example/bogdan/interval_trempo/MainActivity.java
+++ app/src/main/java/com/example/bogdan/interval_trempo/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends Activity {
+
+    private EditText editText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,14 +22,15 @@ public class MainActivity extends Activity {
 
         final ListView listView = (ListView) findViewById(R.id.progressListView);
         Button button = (Button) findViewById(R.id.addButton);
-
+        editText = (EditText) findViewById(R.id.editText);
 
         final List<ProgressInfo> items = new ArrayList<ProgressInfo>();
 
         button.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                items.add(new ProgressInfo(300));
+                items.add(new ProgressInfo(Integer.valueOf(String.valueOf(editText.getText()))));
+                editText.setText("");
                 listView.setAdapter(new MyAdapter(getApplicationContext(), R.id.progressListView, items));
 
             }
